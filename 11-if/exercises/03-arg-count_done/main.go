c1e62e015714297832cb9e501fcf7fdc0b53689b
NicatBabayev <nicatbabayev@gmail.com>
2021-12-20 08:25:22 +0400

    Made Changes

---
+++ 11-if/exercises/03-arg-count_done/main.go
@@ -0,0 +1,46 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Arg Count
+//
+//  1. Get arguments from command-line.
+//  2. Print the expected outputs below depending on the number
+//     of arguments.
+//
+// EXPECTED OUTPUT
+//  go run main.go
+//    Give me args
+//
+//  go run main.go hello
+//    There is one: "hello"
+//
+//  go run main.go hi there
+//    There are two: "hi there"
+//
+//  go run main.go I wanna be a gopher
+//    There are 5 arguments
+// ---------------------------------------------------------
+
+func main() {
+	len := len(os.Args)
+	var message string
+	if len == 1 {
+		fmt.Printf("%s\n", "Give me args")
+	} else if len > 1 {
+		fmt.Printf("There are %d arguments: %s", len-1, os.Args[1:])
+	}
+	fmt.Println(message)
+}
